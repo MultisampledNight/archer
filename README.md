42bdf96375b4e68e73291f17c6002a58d9fc9db9
MultisampledNight <contact@multisamplednight.com>
2021-04-07 15:53:13 +0200

    Initial commit with basic functionality

---
+++ README.md
@@ -0,0 +1,38 @@
+# Archer
+Just a normal Bot for Reaction Roles, thought for the German Arch Linux Discord
+server.
+
+## What are Reaction Roles?
+With Reaction Roles, you can easily let your members on your Discord server give
+roles on their own. For example, let's say there is a #coding-help channel on
+your server, where people can help each other when they have coding problems.
+Let's say, on your server are 2 languages dominant for some reason: `Python` and
+`C`. Then you could set up Reaction Roles to give roles. For example, if a
+question about Python gets asked in #coding-help, it might be just overseen if
+it got no ping at all. But if the OP can directly ping the `Python` role, he can
+receive quick responses. The persons who don't know about Python probably won't
+give the Python role to themselves, so no person is woken up for nothing.
+
+## Hosting instructions
+If you want to use the bot on your own Server, you have to host it on your own.
+Please note that the code is not one of the best. Thus, consider that the bot's
+messages are _all_ German.
+
+```sh
+git clone https://github.com/MultisampledNight/archer
+cd archer
+python3 -m venv archer_venv
+source archer_venv/bin/activate
+pip install -r requirements.txt
+```
+Then, create a new file called `TOKEN` in the future working directory of the
+bot. In there, store ONLY the token you get from the [Discord Developer
+Portal](https://discord.com/developers/applications/) when creating a new
+application. Make sure not to leak it. Afterwards, in the same terminal where
+you created the venv before, do:
+```sh
+python3 main.py
+```
+...and the bot should run now.
+
+
---
+++ main.py
@@ -0,0 +1,296 @@
+import logging
+import random
+import re
+import shlex
+
+import discord
+from discord.utils import get
+
+
+HELP_MSG = """\
+```md
+# ARCHER(1)
+
+## NAME
+    Archer - Einfach nur ein Bot
+
+## SYNOPSIS
+    <prefix><command> [arguments]
+
+## DESCRIPTION
+    Ein Bot für den deutschen Arch Linux Server
+
+## COMMANDS
+    help
+        Zeigt diese Hilfe an.
+
+    prefix <new-prefix>
+        Setzt ein neues Präfix.
+
+    whoami
+        Zeigt an, ob du Einstellungen am Bot verändern darfst.
+
+    dump
+        Dumpt die derzeitigen Einstellungen zu Debuggingzwecken.
+
+    set-mod-role <role-name>
+        Setzt die Moderationsrolle, welche für das Verändern von Einstellungen
+        benötigt wird.
+    
+    send-role-message <channel-id>
+        Sendet die Nachricht mit der Rollenauswahl in den angegebenen Channel.
+
+    add-role <emoji> <role-name>
+        Fügt eine Verlinkung zu der gegebenen Rolle hinzu, welche mithilfe des
+        Emojis bei der Nachricht von send-role-message hinzugefügt werden kann.
+
+    remove-role <emoji>
+        Entfernt die Verlinkung der Rolle mit dem Emoji.
+
+## BUGS
+    Es können nur Custom Emojis als Reaction Roles verwendet werden.
+    Manchmal verselbstständigt er sich. Aber nur manchmal.
+
+## REPORTING BUGS
+    Sende einfach eine Nachricht auf dem Arch Linux Discord, und pinge einen von
+    uns gleich mit. Wir werden es aber (je nach Tageszeit und Tag)
+    wahrscheinlich auch ohne Ping schnell sehen.
+
+## AUTHORS
+    /home/donald4444#3512, TornaxO7#7596, MultisampledNight#2425, 
+
+April 2021
+```
+"""
+LOGFORMAT = "[%(asctime)s] <%(levelname)s> %(message)s"
+EMOJI_REGEX = re.compile("<:.+:([0-9]+)>")
+ARCH_RESPONSES = [
+    "ARCH IS THE BEST!",
+    "Arch ist toll.",
+    "I use Arch btw.",
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARCH!:wq",
+    "arch?"
+]
+
+
+intents = discord.Intents(members=True, emojis=True, messages=True, reactions=True, guilds=True)
+client = discord.Client(intents=intents)
+prefix = "archer "
+roles_msg = None  # the message where roles are given by reactions
+roles_channel = None  # the channel where the message is in
+mod_role = None
+roles = {}  # key is the reaction emoji, value is the role
+
+
+def get_sudo_denied_message(user: discord.Member) -> str:
+    user_formatted = f"{user.name}#{user.discriminator} ({user.id})"
+    logging.warning(f"{user_formatted} failed to authenticate as root.")
+    return f"{user_formatted} ist nicht in der sudoers Datei. Dieser Vorfall wird gemeldet."
+
+
+def user_has_mod_perm(guild: discord.Guild, user_id: int) -> bool:
+    user = guild.get_member(user_id)
+    return user_id == 817468254255054908 or mod_role in user.roles
+
+
+@client.event
+async def on_ready():
+    logging.info(f"Login as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    global prefix
+    global mod_role
+    global roles_msg
+    global roles_channel
+    if message.author == client.user:
+        return
+
+    if message.guild is None:
+        await message.channel.send("Du kannst diesen Bot nicht in Direktnachrichten benutzen.")
+        return
+
+    if message.content.startswith(prefix):
+        command = message.content[len(prefix):]  # strip the prefix
+        command = shlex.split(command)  # shlex allows easy shell-like parsing
+
+        logging.info(f"Command issued by {message.author.name}#{message.author.discriminator}: {command}")
+
+        if not command:  # e.g. just 'archer' or 'archer '
+            return
+
+        elif command[0].lower() == "help":
+            await message.channel.send(HELP_MSG)
+
+        elif command[0].lower() == "prefix":
+            if not user_has_mod_perm(message.guild, message.author.id):
+                await message.channel.send(get_sudo_denied_message(message.author))
+                return
+
+            if len(command) < 2:
+                await message.channel.send("Kein neues Präfix angegeben.")
+                return
+            prefix = command[1]
+            await message.channel.send(f"Neues Präfix ist nun `{prefix}`.")
+
+        elif command[0].lower() == "dump":
+            if not user_has_mod_perm(message.guild, message.author.id):
+                await message.channel.send(get_sudo_denied_message(message.author))
+                return
+
+            await message.channel.send(f"""\
+```
+prefix: {prefix}
+mod_role: {mod_role}
+roles_msg: {roles_msg}
+roles_channel: {roles_channel}
+roles: {roles}
+```""")
+        
+        elif command[0].lower() == "whoami":
+            if user_has_mod_perm(message.guild, message.author.id):
+                await message.channel.send(f"Du darfst Einstellungen vornehmen.")
+            else:
+                await message.channel.send(f"Du darfst keine Einstellungen vornehmen.")
+
+        elif command[0].lower() == "set-mod-role":
+            if not user_has_mod_perm(message.guild, message.author.id):
+                await message.channel.send(get_sudo_denied_message(message.author))
+                return
+
+            role = get(message.guild.roles, name=command[1])
+            if role is None:
+                await message.channel.send("Diese Rolle scheint es nicht zu geben.")
+                return
+
+            mod_role = role
+            await message.channel.send("Moderator-Rolle erfolgreich gesetzt.")
+
+        elif command[0].lower() == "send-role-message":
+            if not user_has_mod_perm(message.guild, message.author.id):
+                await message.channel.send(get_sudo_denied_message(message.author))
+                return
+
+            if len(command) < 2:
+                await message.channel.send("Kein Channel angegeben.")
+                return
+            if not command[1].isdigit():
+                await message.channel.send("Die Channel-ID scheint keine Zahl zu sein. IDs in Discord sind immer Zahlen.")
+                return
+            channel = client.get_channel(int(command[1]))
+            if channel is None:
+                await message.channel.send("Der Channel scheint nicht zu existieren.")
+                return
+
+            message = await channel.send("Benutze die Reaktionen unter dieser Nachricht, um dir selber Rollen zu geben.")
+            roles_msg = message.id
+            roles_channel = channel.id
+            # add reactions to easily click on them
+            for emoji in map(lambda id: get(message.guild.emojis, id=int(id)), roles.keys()):
+                await message.add_reaction(emoji)
+
+        elif command[0].lower() == "add-role":
+            if not user_has_mod_perm(message.guild, message.author.id):
+                await message.channel.send(get_sudo_denied_message(message.author))
+                return
+
+            if len(command) < 3:
+                await message.channel.send("Es wurden zu wenig Argumente angegeben.")
+                return
+
+            emoji_match = EMOJI_REGEX.match(command[1])
+            if emoji_match is None:
+                await message.channel.send("Das erste Argument scheint kein custom Emoji sein.")
+                return
+            emoji_id = emoji_match.group(1)
+
+            role = get(message.guild.roles, name=command[2])
+            if role is None:
+                await message.channel.send("Diese Rolle scheint es nicht zu geben.")
+                return
+            if role in roles.values():
+                await message.channel.send("Die Rolle ist bereits verlinkt.")
+                return
+
+            roles[emoji_id] = role
+            await message.channel.send("Rolle verlinkt.")
+
+            # add the new role to the message, if it was sent yet
+            if roles_msg is None:
+                return
+            channel = client.get_channel(roles_channel)
+            message = await channel.fetch_message(roles_msg)
+            emoji = get(message.guild.emojis, id=int(emoji_id))
+            await message.add_reaction(emoji)
+
+
+        elif command[0].lower() == "remove-role":
+            if not user_has_mod_perm(message.guild, message.author.id):
+                await message.channel.send(get_sudo_denied_message(message.author))
+                return
+
+            if len(command) < 2:
+                await message.channel.send("Kein Emoji angegeben.")
+                return
+
+            emoji_match = EMOJI_REGEX.match(command[1])
+            if emoji_match is None:
+                await message.channel.send("Das Argument scheint kein custom Emoji sein.")
+                return
+            emoji_id = emoji_match.group(1)
+
+            if emoji_id not in roles.keys():
+                await message.channel.send("Es gibt gar keine Rolle für diesen Emoji.")
+                return
+
+            del roles[emoji_id]
+            await message.channel.send("Rolle gelöscht.")
+
+            if roles_msg is None:
+                return
+            channel = client.get_channel(roles_channel)
+            message = await channel.fetch_message(roles_msg)
+            emoji = get(message.guild.emojis, id=int(emoji_id))
+            await message.remove_reaction(emoji, message.author)
+
+        else:
+            await message.channel.send(f"Unbekannter Befehl. Benutze `{prefix}help` für Hilfe.")
+
+    elif "arch" in message.content.lower():
+        await message.channel.send(random.choice(ARCH_RESPONSES))
+
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.user_id == client.user.id:
+        # avoid applying roles to self
+        return
+
+    emoji_matches = str(payload.emoji.id) in roles.keys()
+    message_matches = payload.message_id == roles_msg
+    if emoji_matches and message_matches:
+        # can't use get_user here because we need a Member, not a User
+        guild = client.get_guild(payload.guild_id)
+        member = guild.get_member(payload.user_id)
+        role = roles[str(payload.emoji.id)]
+        await member.add_roles(role, reason="Automatically through Reaction Roles")
+
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    emoji_matches = str(payload.emoji.id) in roles.keys()
+    message_matches = payload.message_id == roles_msg
+    if emoji_matches and message_matches:
+        guild = client.get_guild(payload.guild_id)
+        member = guild.get_member(payload.user_id)
+        role = roles[str(payload.emoji.id)]
+        await member.remove_roles(role, reason="Automatically through Reaction Roles")
+
+
+if __name__ == "__main__":
+    with open("TOKEN") as fh:
+        token = fh.read()
+
+    logging.basicConfig(encoding="utf-8", format=LOGFORMAT, level=logging.INFO)
+    client.run(token)
